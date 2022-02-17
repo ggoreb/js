831aab6546eaf6606c4aa13851fa590561bc705e
GGoReb <seorab@naver.com>
2022-02-17 14:13:06 +0900

    js 객체

---
+++ main.js
@@ -0,0 +1,9 @@
+import {plus} from './module.js';
+import m from './module.js';
+
+console.log(plus());
+console.log(m());
+
+export default function run() {
+  alert(111);
+}
---
+++ module_ex.js
@@ -0,0 +1,7 @@
+console.log('ex!!');
+
+// import export와 같이
+// 새로 나온 문법 X
+function action() {
+  alert(222);
+}
